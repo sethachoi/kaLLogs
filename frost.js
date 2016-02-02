04c463d0a5831c73c07eed92ef34759454553473
Seth <sac061@ucsd.edu>
2016-02-02 15:30:05 -0800

    changed like, a lot of stuff so it actually makes sense now

--- frost.js
+++ frost.js
@@ -1,21 +1,45 @@
-
+var dataset = [{
+	        data: [{
+	            month: 'Aug',
+	            count: 123
+	        	}, {
+	            month: 'Sep',
+	            count: 234
+	        	}, {
+	            month: 'Oct',
+	            count: 345
+        	}], 
+        	name: 'Series #1' }, 
+        	{
+        	data: [{
+            	month: 'Aug',
+            	count: 235
+        		}, {
+            	month: 'Sep',
+            	count: 267
+       			}, {
+            	month: 'Oct',
+            	count: 573
+        	}],
+        name: 'Series #2'
+    	}];
 
 $(document).ready(function() {
 	$('.dropdown-toggle').dropdown();
-	doEverything("memChart");
-	doEverything("cpuChart");
-	doEverything("ioChart");
+	render();
 });
 
 function render() {
-	console.log("lol");
+	doEverything("memChart",dataset,"scal");
+	doEverything("cpuChart",dataset,"perc");
+	doEverything("ioChart",dataset,"scal");
 }
 
 function getInfo() {
 	console.log("lel");
 }
 
-function doEverything(target_elem) {
+function doEverything(target_elem, data_set, type) {
 	var margins = {
 	    top: 12,
 	    left: 48,
@@ -26,26 +50,11 @@ function doEverything(target_elem) {
 	    width: 180
 	},
 	width = 500 - margins.left - margins.right - legendPanel.width,
-	    height = 100 - margins.top - margins.bottom,
-	    dataset = [{
-	        data: [{
-	            month: 'Aug',
-	            count: 123
-	        }],
-	        name: 'Series #1'
-	    }, {
-	        data: [{
-	            month: 'Aug',
-	            count: 235
-	        }],
-	        name: 'Series #2'
-	    }
-
-	    ],
-	    series = dataset.map(function (d) {
+	    height = 200 - margins.top - margins.bottom,
+	    series = data_set.map(function (d) {
 	        return d.name;
 	    }),
-	    dataset = dataset.map(function (d) {
+	    data_set = data_set.map(function (d) {
 	        return d.data.map(function (o, i) {
 	            // Structure it so that your numeric
 	            // axis (the stacked amount) is y
@@ -57,9 +66,9 @@ function doEverything(target_elem) {
 	    }),
 	    stack = d3.layout.stack();
 
-	stack(dataset);
+	stack(data_set);
 
-	var dataset = dataset.map(function (group) {
+	var data_set = data_set.map(function (group) {
 	    return group.map(function (d) {
 	        // Invert the x and y values, and y0 becomes x0
 	        return {
@@ -69,103 +78,87 @@ function doEverything(target_elem) {
 	        };
 	    });
 	}),
-	    svg = d3.select('#' + target_elem)
-	        .append('svg')
-	        .attr('width', width + margins.left + margins.right + legendPanel.width)
-	        .attr('height', height + margins.top + margins.bottom)
-	        .classed('chartFiller', true)
-	        .append('g')
-	        .attr('transform', 'translate(' + margins.left + ',' + margins.top + ')'),
-	    xMax = d3.max(dataset, function (group) {
-	        return d3.max(group, function (d) {
-	            return d.x + d.x0;
-	        });
-	    }),
-	    xScale = d3.scale.linear()
-	        .domain([0, xMax])
-	        .range([0, width]),
-	    months = dataset[0].map(function (d) {
-	        return d.y;
-	    }),
-	    _ = console.log(months),
-	    yScale = d3.scale.ordinal()
-	        .domain(months)
-	        .rangeRoundBands([0, height], .1),
-	    xAxis = d3.svg.axis()
-	        .scale(xScale)
-	        .orient('bottom'),
-	    yAxis = d3.svg.axis()
-	        .scale(yScale)
-	        .orient('left'),
-	    colours = d3.scale.category10(),
-	    groups = svg.selectAll('g')
-	        .data(dataset)
-	        .enter()
-	        .append('g')
-	        .style('fill', function (d, i) {
-	        return colours(i);
-	    }),
-	    rects = groups.selectAll('rect')
-	        .data(function (d) {
-	        return d;
-	    })
-	        .enter()
-	        .append('rect')
-	        .attr('x', function (d) {
-	        return xScale(d.x0);
-	    })
-	        .attr('y', function (d, i) {
-	        return yScale(d.y);
-	    })
-	        .attr('height', function (d) {
-	        return yScale.rangeBand();
-	    })
-	        .attr('width', function (d) {
-	        return xScale(d.x);
-	    })
-	        .on('mouseover', function (d) {
-	        var xPos = parseFloat(d3.select(this).attr('x')) / 2 + width / 2;
-	        var yPos = parseFloat(d3.select(this).attr('y')) + yScale.rangeBand() / 2;
+    svg = d3.select('#' + target_elem)
+        .append('svg')
+        .attr('width', width + margins.left + margins.right + legendPanel.width)
+        .attr('height', height + margins.top + margins.bottom)
+        .classed('chartFiller', true)
+        .append('g')
+        .attr('transform', 'translate(' + margins.left + ',' + margins.top + ')'),
+    xMax = d3.max(data_set, function (group) {
+        return d3.max(group, function (d) {
+            return d.x + d.x0;
+        });
+    }),
+    xScale = d3.scale.linear()
+        .domain([0, xMax])
+        .range([0, width]),
+    months = data_set[0].map(function (d) {
+        return d.y;
+    }),
+    _ = console.log(months),
+    yScale = d3.scale.ordinal()
+        .domain(months)
+        .rangeRoundBands([0, height], .1),
+    xAxis = d3.svg.axis()
+        .scale(xScale)
+        .orient('bottom'),
+    yAxis = d3.svg.axis()
+        .scale(yScale)
+        .orient('left'),
+    colours = d3.scale.category10(),
+    groups = svg.selectAll('g')
+        .data(data_set)
+        .enter()
+        .append('g')
+        .style('fill', function (d, i) {
+        return colours(i);
+    }),
+    rects = groups.selectAll('rect')
+        .data(function (d) {
+        return d;
+    })
+        .enter()
+        .append('rect')
+        .attr('x', function (d) {
+        return xScale(d.x0);
+    })
+        .attr('y', function (d, i) {
+        return yScale(d.y);
+    })
+        .attr('height', function (d) {
+        return yScale.rangeBand();
+    })
+        .attr('width', function (d) {
+        return xScale(d.x);
+    })
+        .on('mouseover', function (d) {
+        	/*
+        var xPos = parseFloat(d3.select(this).attr('x')) / 2 + width / 2;
+        var yPos = parseFloat(d3.select(this).attr('y')) + yScale.rangeBand() / 2;
+			*/
+		var coordinates = [0, 0];
+		coordinates = d3.mouse(document.body);
+		var xPos = coordinates[0];
+		var yPos = coordinates[1];
+        d3.select('#tooltip')
+            .style('left', xPos + 'px')
+            .style('top', yPos + 'px')
+            .select('#value')
+            .text(d.y + ": " + d.x);
 
-	        d3.select('#tooltip')
-	            .style('left', xPos + 'px')
-	            .style('top', yPos + 'px')
-	            .select('#value')
-	            .text(d.x);
+        d3.select('#tooltip').classed('hidden', false);
+    })
+        .on('mouseout', function () {
+        d3.select('#tooltip').classed('hidden', true);
+    })
 
-	        d3.select('#tooltip').classed('hidden', false);
-	    })
-	        .on('mouseout', function () {
-	        d3.select('#tooltip').classed('hidden', true);
-	    })
-
-	    svg.append('g')
-	        .attr('class', 'axis')
-	        .attr('transform', 'translate(0,' + height + ')')
-	        .call(xAxis);
-
+    svg.append('g')
+        .attr('class', 'axis')
+        .attr('transform', 'translate(0,' + height + ')')
+        .call(xAxis);
+	        
 	svg.append('g')
 	    .attr('class', 'axis')
 	    .call(yAxis);
-
-	svg.append('rect')
-	    .attr('fill', 'yellow')
-	    .attr('width', 160)
-	    .attr('height', 30 * dataset.length)
-	    .attr('x', width + margins.left)
-	    .attr('y', 0);
-
-	series.forEach(function (s, i) {
-	    svg.append('text')
-	        .attr('fill', 'black')
-	        .attr('x', width + margins.left + 8)
-	        .attr('y', i * 24 + 24)
-	        .text(s);
-	    svg.append('rect')
-	        .attr('fill', colours(i))
-	        .attr('width', 60)
-	        .attr('height', 20)
-	        .attr('x', width + margins.left + 90)
-	        .attr('y', i * 24 + 6);
-	});
 }
